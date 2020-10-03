07265df075a3f87db41c3cb8184508b1b4014f69
andreasonny83 <andreasonny83@gmail.com>
2020-10-03 14:09:00 +0100

    Add Yarn

--- terminal-sync.sh
+++ terminal-sync.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 set +x
 
-PWD=${PWD}
+# TODO: Git; Git config; .ssh; aws; npmrc; vim; vimconfig
 
-# download config from server
-# Git
-# Git config
-# .ssh
+PWD=${PWD}
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+NC="\033[0m"
+
+function AskQuestion ()
+{
+  local res
+  echo -e "\n"
+  read -p "$(echo -e $GREEN"?"$NC" $1? (Y/n) ")" res
+  if [[ -z "$res" ]] || [[ "${res:0:1}" == "y" ]] || [[ "${res:0:1}" == "Y" ]]; then
+    if [[ ! -z "$2" ]]; then
+      echo "$2 ..."
+    fi
+    return 1
+  fi
+
+  return 0
+}
 
 echo "Checking XCode Tools..."
 
@@ -16,80 +33,70 @@ else
   xcode-select --install
 fi
 
-read -p "Enter the URL containing your Terminal Sync configuration " terminalsyncconfig
+echo -e "\n"
+read -p "Enter the URL containing your Terminal Sync configuration: " terminalsyncconfig
 curl -fsSL $terminalsyncconfig > $PWD/.terminal-sync-tmp.sh
 source $PWD/.terminal-sync-tmp.sh
 rm $PWD/.terminal-sync-tmp.sh
 
-echo -e "\n"
-read -p  "Do you want to install Homebrew? (Y/n) " homebrew
-
-if [[ -z "$homebrew" ]] || [[ "${homebrew:0:1}" == "y" ]] || [[ "${homebrew:0:1}" == "Y" ]]; then
-  echo "Installing Homebrew..."
+AskQuestion "Do you want to install Homebrew" "Installing Homebrew"
+if [[ "$?" == 1 ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-echo -e "\n"
-read -p "Do you want to install iTerm2 (This requires Homebrew to be installed)? (Y/n) " iterm
-
-if [[ -z "$iterm" ]] || [[ "${iterm:0:1}" == "y" ]] || [[ "${iterm:0:1}" == "Y" ]]; then
-  echo "Installing iTerm2..."
+AskQuestion "Do you want to install iTerm2 (This requires Homebrew to be installed)" "Installing iTerm2"
+if [[ "$?" == 1 ]]; then
   brew cask install iterm2
 fi
 
-echo -e "\n"
-read -p "Do you want to download your custom iTerm2 configuration? (Y/n) " itermconfig
-
-if [[ -z "$itermconfig" ]] || [[ "${itermconfig:0:1}" == "y" ]] || [[ "${itermconfig:0:1}" == "Y" ]]; then
+AskQuestion "Do you want to download your custom iTerm2 configuration"
+if [[ "$?" == 1 ]]; then
   if [[ -d "$HOME/itermConfig" ]]; then
     echo "itermConfig folder already exists"
   else
     mkdir $HOME/itermConfig
   fi
-
   curl $itermconfigurl > $HOME/itermConfig/iterm2.plist
-  echo "iTerm configuration file saved to $HOME/itermConfig/iterm2.plist"
+  echo -e "\n${BLUE}iTerm configuration file saved to $HOME/itermConfig/iterm2.plist${NC}"
 fi
 
-echo -e "\n"
-read -p "Do you want to install Oh-my-zsh? (Y/n) " ohmyzsh
-
-if [[ -z "$ohmyzsh" ]] || [[ "${ohmyzsh:0:1}" == "y" ]] || [[ "${ohmyzsh:0:1}" == "Y" ]]; then
-  echo "You will need to restart your terminal and run this script again after Oh-my-zsh is installed."$'\n'"(Press any key to continue) " anykey
-  echo "Installing Oh-my-zsh..."
+AskQuestion "Do you want to install Oh-my-zsh"
+if [[ "$?" == 1 ]]; then
+  echo -e "\n${RED}You will need to restart your terminal and run this script again after Oh-my-zsh is installed.${NC}"
+  read -p "(Press any key to continue) " anykey
+  echo -e "\nInstalling Oh-my-zsh..."
   /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-echo -e "\n"
-read -p "Do you want to import your existing zsh configuration? (Y/n) " importzshell
-
-if [[ -z "$importzshell" ]] || [[ "${importzshell:0:1}" == "y" ]] || [[ "${importzshell:0:1}" == "Y" ]]; then
+AskQuestion "Do you want to import your existing zsh configuration"
+if [[ "$?" == 1 ]]; then
   curl $zshrc > $HOME/.zshrc
 
-  echo -e "\n"
-  read -p "Do you want to install your custom terminal plugins and themes? (Y/n) " zshplugins
-
-  if [[ -z "$zshplugins" ]] || [[ "${zshplugins:0:1}" == "y" ]] || [[ "${zshplugins:0:1}" == "Y" ]]; then
+  AskQuestion "Do you want to install your custom terminal plugins and themes"
+  if [[ "$?" == 1 ]]; then
     /bin/bash -c "$(curl -fsSL $zshpluginsurl)"
   fi
 fi
 
-echo -e "\n"
-read -p "Do you want to install Terminal Fonts? (Y/n) " fonts
+AskQuestion "Do you want to install Yarn" "Installing Yarn"
+if [[ "$?" == 1 ]]; then
+  brew install yarn
+fi
 
-if [[ -z "$fonts" ]] || [[ "${fonts:0:1}" == "y" ]] || [[ "${fonts:0:1}" == "Y" ]]; then
+AskQuestion "Do you want to install Terminal Fonts"
+if [[ "$?" == 1 ]]; then
   echo "brew tap homebrew/cask-fonts"
   brew update --force --verbose
   brew tap homebrew/cask-fonts
-  echo "Installing font meslo..."
+  echo "Installing font Meslo..."
   brew cask install font-meslo-lg
   brew cask install font-meslo-for-powerline
-  echo "Installing font hack-nerd..."
+  echo "Installing font Hack-Nerd..."
   brew cask install font-hack-nerd-font
-  echo "Installing font fira-code..."
+  echo "Installing font Fira-Code..."
   brew cask install font-fira-code-nerd-font
   brew cask install font-fira-code
 fi
 
-echo "done"
+echo -e "\n\n${GREEN}All Done!${NC}\n\n"
 exit 0
